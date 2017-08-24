fda5e2ef0ec99b3e1b268b5ba5e1b26ca6da2d30
Siqueira <silvio.siqueira@accenture.com>
2017-08-24 19:30:53 -0300

    modulo de editar e remover as fotos

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -1,8 +1,42 @@
-angular.module('alurapic').controller('FotoController', function($scope){
+angular.module('alurapic').controller('FotoController', function($scope, $http, $routeParams){
 	
-	$scope.foto = {}
+	$scope.foto = {};
+	$scope.mensagem = '';
+
+	if($routeParams.fotoId){
+		$http.get('v1/fotos/' + $routeParams.fotoId)
+		.success(function(foto){
+			$scope.foto = foto;
+		})
+		.error(function(erro){
+			console.log(erro)
+			$scope.mensagem = 'Não foi possível obter a foto';
+		})
+	}
 
 	$scope.submeter = function(){
-		console.log($scope.foto);
-	}
-})
+		if($scope.formulario.$valid){
+			if($scope.foto._id){
+				$http.put('v1/fotos/' + $scope.foto._id, $scope.foto)
+				.success(function(){
+					$scope.mensagem = 'A foto ' + $scope.foto.titulo + ' foi alterada com sucesso';
+				})
+				.error(function(error){
+					console.log(error);
+					$scope.mensagem = 'Não foi possível alterar a foto' + $scope.foto.titulo;
+				})
+			}else{
+				$http.post('v1/fotos', $scope.foto)
+				.success(function(){
+					$scope.mensagem = 'Foto incluída com sucesso';
+				})
+				.error(function(erro){
+					$scope.mensagem = 'Não foi possivel incluir sua foto';
+					console.log(erro);
+				});
+			}
+			
+		}
+		
+	};
+});
